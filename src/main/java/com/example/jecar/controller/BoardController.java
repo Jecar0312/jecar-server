893dd2692c1d59bf6b9db6a5bbf39287431a3cf4
qww1552 <qww1552@naver.com>
2021-03-16 17:08:17 +0900

    API Document 작성

--- src/main/java/com/example/jecar/controller/BoardController.java
+++ src/main/java/com/example/jecar/controller/BoardController.java
@@ -5,11 +5,14 @@ import com.example.jecar.dto.Board;
 import com.example.jecar.dto.Reply;
 import com.example.jecar.repository.BoardRepository;
 import com.example.jecar.service.ReplyService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"2. Board"})
 @RestController
 @RequestMapping(value = "/boards")
 public class BoardController {
@@ -26,11 +29,13 @@ public class BoardController {
     }
 
     ///// Board
+    @ApiOperation(value = "게시글 조회", notes = "모든 게시글을 조회한다.")
     @GetMapping("/")
     public List<Board> list() {
         return boardRepository.findAll();
     }
 
+    @ApiOperation(value = "단일 게시글 조회", notes = "단일 게시글과 해당 게시글에 달린 댓글들을 조회한다.")
     @GetMapping("/{id}")
     public APIBoard get(@PathVariable Integer id) {
         Board board = boardRepository.findById(id).get();
@@ -38,16 +43,19 @@ public class BoardController {
         return new APIBoard(board, replyList);
     }
 
+    @ApiOperation(value = "게시글 생성", notes = "게시글을 생성한다.")
     @PostMapping("/")
     public Board create(@ModelAttribute Board board) {
         return boardRepository.save(board);
     }
 
+    @ApiOperation(value = "게시글 삭제", notes = "해당하는 게시글을 삭제한다.")
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Integer id) {
         boardRepository.delete(boardRepository.findById(id).get());
     }
 
+    @ApiOperation(value = "게시글 수정", notes = "해당하는 게시글을 수정한다.")
     @PutMapping("/{id}")
     public Board modify(@PathVariable Integer id, @ModelAttribute final Board updates) {
         Board board = boardRepository.findById(id).get();
@@ -58,6 +66,7 @@ public class BoardController {
     /////
 
     ///// Reply
+    @ApiOperation(value = "댓글 생성", notes = "댓글을 생성한다.")
     @PostMapping("/{boardId}/reply")
     public Reply createReply(@ModelAttribute Reply reply, @PathVariable Integer boardId) {
         reply.setBoard(boardRepository.findById(boardId).get());
--- src/main/java/com/example/jecar/controller/MemberController.java
+++ src/main/java/com/example/jecar/controller/MemberController.java
@@ -2,11 +2,14 @@ package com.example.jecar.controller;
 
 import com.example.jecar.dto.Member;
 import com.example.jecar.repository.MemberRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"1. Member"})
 @RestController
 @RequestMapping("/members")
 public class MemberController {
@@ -19,21 +22,25 @@ public class MemberController {
         this.memberRepository = memberRepository;
     }
 
+    @ApiOperation(value = "회원 조회", notes = "모든 회원 정보를 조회한다.")
     @GetMapping("/")
     public List<Member> list() {
         return memberRepository.findAll();
     }
 
+    @ApiOperation(value = "단일 회원 조회", notes = "회원 정보 한 건을 조회한다.")
     @GetMapping("/{id}")
     public Member get(@PathVariable Integer id) {
         return memberRepository.findById(id).get();
     }
 
+    @ApiOperation(value = "회원 생성", notes = "회원 정보를 생성한다.")
     @PostMapping("/")
     public Member create(@ModelAttribute Member member) {
         return memberRepository.save(member);
     }
 
+    @ApiOperation(value = "회원 수정", notes = "해당하는 회원 정보를 수정한다.")
     @PutMapping("/{id}")
     public Member modify(@PathVariable Integer id, @RequestBody Member updates) {
         Member member = memberRepository.findById(id).get();
@@ -43,6 +50,7 @@ public class MemberController {
         return memberRepository.save(member);
     }
 
+    @ApiOperation(value = "회원 삭제", notes = "해당하는 회원 정보를 삭제한다.")
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Integer id) {
         memberRepository.delete(memberRepository.getOne(id));
